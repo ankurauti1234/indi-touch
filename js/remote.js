d2229f209ef40bee647d64b83202e369c3eaedb7
Mateen Mushrif <mateenmushrif04@gmail.com>
2026-07-10 17:34:40 +0530

    group member ui update

--- js/remote.js
+++ js/remote.js
@@ -35,7 +35,7 @@ function setFocus(el) {
     if (!el) return;
 
     el.classList.add('remoteFocused');
-    console.log('Remote Focus:', el);
+    // console.log('Remote Focus:', el);
 
     // ANTI-STRETCH CHECK
     const isInsidePopup = el.closest(

1a7f02cef1ac7b471d142310f3b8626c9f5654fb
Mateen Mushrif <mateenmushrif04@gmail.com>
2026-07-13 12:16:17 +0530

    additional fixes

--- js/remote.js
+++ js/remote.js
@@ -546,7 +546,23 @@ export function initRemote() {
         }
     });
 
-    document.addEventListener('mousemove', () => {
-        if (isRemoteMode()) clearFocus();
+    // --- THE SYNTHETIC MOUSE FIX ---
+    let lastMouseX = -1;
+    let lastMouseY = -1;
+
+    document.addEventListener('mousemove', (e) => {
+        if (!isRemoteMode()) return;
+
+        // If the mouse hasn't actually changed physical pixels, it's a synthetic
+        // event caused by the page scrolling. Ignore it completely!
+        if (e.screenX === lastMouseX && e.screenY === lastMouseY) {
+            return; 
+        }
+
+        lastMouseX = e.screenX;
+        lastMouseY = e.screenY;
+
+        // If it was a real physical mouse movement, clear the TV remote focus
+        clearFocus();
     });
 }

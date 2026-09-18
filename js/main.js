7466106919b4b079e7b0ca765658024e8a6236fb
Mateen Mushrif <mateenmushrif04@gmail.com>
2026-09-18 16:18:01 +0530

    10AM and 6PM reset removed , testing for 2AM

--- js/main.js
+++ js/main.js
@@ -100,7 +100,7 @@ async function runDailyMaintenanceIfNeeded() {
 
     if (
         currentHour > 14 ||
-        (currentHour === 14 && currentMinute >= 47)
+        (currentHour === 14 && currentMinute >= 50)
     ) {
         resetSlot = "02:00";
     } else {
